68203a7958f522e4c6d018c1036bc71c087e4935
oleg-abrashin <oabrashin@gmail.com>
2019-06-11 12:58:21 +0300

    Magellan project commit

--- Project_magellan_11_06_19/src/Main.java
+++ Project_magellan_11_06_19/src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        System.out.println("Hello World from Project Magellan 11-06-2019 !");
     }
 }
--- Project_magellan_11_06_19/src/application/MagellanApp.java
+++ Project_magellan_11_06_19/src/application/MagellanApp.java
@@ -2,6 +2,31 @@ package application;
 
 public class MagellanApp {
 
-    
+    //psvm
+
+    public static void main(String[] args) {
+
+        int ship1 = 1;
+        int ship2 = 0;
+
+        while(ship1 > ship2){
+            ship1++;
+            ship2++;
+        }
+
+        ship1 = 50000;
+        ship2 = 50000 * 50000;
+
+        System.out.println(ship1);
+        System.out.println(ship2);
+        //sout  
+
+    }
+
+
+
+
+
+
 
 }
