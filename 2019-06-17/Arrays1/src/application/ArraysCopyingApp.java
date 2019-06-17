dcd7d091eaf9a8c9c67a690a97cf40515cb7583b
oleg-abrashin <oabrashin@gmail.com>
2019-06-17 10:22:35 +0300

    Arrays Copying

--- 2019-06-17/Arrays1/src/application/ArraysCopyingApp.java
+++ 2019-06-17/Arrays1/src/application/ArraysCopyingApp.java
@@ -12,25 +12,46 @@ public class ArraysCopyingApp {
     public static void main(String[] args) {
 
         int[] arr = new int[NUM_NUM];
+
         for (int i=0; i < arr.length; i++){
 
-            arr[i] = gen.nextInt(10);
+            arr[i] = 10+gen.nextInt(10);
 
         }
 
-        System.out.println(Arrays.toString(arr));
+          System.out.println(Arrays.toString(arr));
 
-        int[] copy = Arrays.copyOf(arr, 25);
-
-        System.out.println(Arrays.toString(copy));
-
-        int[] range = Arrays.copyOfRange(arr,15,25);
-
-        System.out.println(Arrays.toString(range));
+//        int[] copy = Arrays.copyOf(arr, 25);
+//
+//        System.out.println(Arrays.toString(copy));
+//
+//        int[] range = Arrays.copyOfRange(arr,5,10);
+//
+//        System.out.println(Arrays.toString(range));
 
 //        range[2] = 444;
 //        System.out.println(Arrays.toString(range));
 //        System.out.println(Arrays.toString(arr));
+
+//        int dest[] = new int[NUM_NUM/2];
+//
+//        System.arraycopy(arr,3,dest,5,3);
+//
+//        System.out.println(Arrays.toString(dest));
+
+          System.out.println(Arrays.toString( remove(arr,10)));
+
+
+
+    }
+
+    public static int[] remove(int[] arr, int index){
+
+        int len1 = arr.length-1;
+
+        System.arraycopy(arr, index+1, arr, index, len1 - index);
+
+        return Arrays.copyOf(arr,len1);
 
     }
 
