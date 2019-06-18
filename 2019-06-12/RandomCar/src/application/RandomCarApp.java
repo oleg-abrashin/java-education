16e7f3a3e771f27d0df659c9f7c6cfcbc97f4cb2
oleg-abrashin <oabrashin@gmail.com>
2019-06-18 09:35:31 +0300

    18-06-19

--- 2019-06-12/RandomCar/src/application/RandomCarApp.java
+++ 2019-06-12/RandomCar/src/application/RandomCarApp.java
@@ -6,14 +6,14 @@ public class RandomCarApp {
     private static final int NUM_CARS = 5;
 
     public static void main(String[] args) {
-        Car[] garage = new Car[NUM_CARS];
+        //Car[] garage = new Car[NUM_CARS];
 
-//        //input random cars in garage
-        for (int i = 0; i < garage.length; i++) {
-            garage[i] = Car.randomCar();
-        }
+////        //input random cars in garage
+//        for (int i = 0; i < garage.length; i++) {
+//            garage[i] = Car.randomCar();
+//        }
 
-        //Car.getRandomGarageArray(50);
+        Car[] garage = Car.getRandomGarageArray(50);
 
         //output cars from garage
         for (int i = 0; i < garage.length; i++) {
