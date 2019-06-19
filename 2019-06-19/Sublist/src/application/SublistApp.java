ef3de0461e02a9f6d4dc078d11dd84df74ff1b54
oleg-abrashin <oabrashin@gmail.com>
2019-06-19 11:05:59 +0300

    added project with trap in view of arraylist

---
+++ 2019-06-19/Sublist/src/application/SublistApp.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class SublistApp {
+
+    private static final int NUM_NUM = 10;
+    static Random gen = new Random();
+
+    public static void main(String[] args) {
+        ArrayList<Integer> ali = new ArrayList<>();
+
+        for (int i = 0; i < NUM_NUM; i++)ali.add(gen.nextInt(10));
+        System.out.println("ali = "+ali);
+
+        List<Integer> sub = ali.subList(2,7);
+
+        //ArrayList<Integer> sub = new ArrayList<>();
+
+
+        System.out.println("sub = "+sub);
+
+        sub.set(2, 777);
+        System.out.println("ali = "+ali);
+        System.out.println("sub = "+sub);
+    }
+
+}
